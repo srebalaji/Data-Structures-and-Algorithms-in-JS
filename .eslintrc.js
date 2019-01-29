ea258f31ce0edacfd3ff0f5aba773b84da604d4d
Srebalaji Thirumalai <srebalaji@outlook.com>
2019-01-29 22:19:16 +0530

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": ["error", "never"],
+        "linebreak-style": 0,
+        "no-param-reassign": 0,
+        "consistent-return": 0,
+        "no-bitwise": 0,
+        "indent": [2, 2],
+        "space-before-function-paren": ["error", 'always'],
+        "no-underscore-dangle": 0,
+        "no-prototype-builtins": 0
+    },
+    "env": {
+        "es6": true,
+        "node": true
+    }
 };
